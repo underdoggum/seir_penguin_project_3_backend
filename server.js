076f6bb39954666149de98e1613ed16cb7cb751b
Lucy Liu <lucy.liu94@gmail.com>
2021-11-22 11:50:16 -0500

    revert changes

--- server.js
+++ server.js
@@ -1,63 +1,39 @@
 ///////////////////////////////
 // DEPENDENCIES
 ////////////////////////////////
-
 require("dotenv").config()
-const { PORT = 4000, DATABASE_URL } = process.env
+const { PORT = 4000,  DATABASE_URL} = process.env
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const morgan = require("morgan")
 const cors = require("cors")
-
-
+const config = {useUnifiedTopology: true, useNewUrlParser: true,}
+const AuthRouter = require("./controllers/user")
 ///////////////////////////////
-// DATABASE CONNECTION
+// MIDDLEWARE
 ////////////////////////////////
-// Establish Connection
-mongoose.connect(DATABASE_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
+app.use(express.json())
+app.use(cors());
+app.use(morgan("tiny"));
+const auth = require("./auth")
+/////////////////////////////
+// DATABASE CONNECTION
+//////////////////////////////
+mongoose.connect(DATABASE_URL, config)
 // Connection Events
 mongoose.connection
   .on("open", () => console.log("You are connected to mongoose"))
   .on("close", () => console.log("You are disconnected from mongoose"))
   .on("error", (error) => console.log(error));
-
-
-///////////////////////////////
-// User Schema for Authentication
-////////////////////////////////
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  password: {
-    type: String,
-    required: true.valueOf,
-  }
-   
-})
-
-const User = mongoose.model("User", UserSchema)
-
-
-
 ///////////////////////////////
 // Items Schema
 ////////////////////////////////
-
-
 const ItemSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
@@ -74,66 +50,65 @@ const ItemSchema = new mongoose.Schema({
   description: String,
   img: String
 })
-
 const Item = mongoose.model("Item", ItemSchema)
-
 ///////////////////////////////
 // MIDDLEWARE
 ////////////////////////////////
-
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
-
-
 ///////////////////////////////
 // SEED DATA
 ////////////////////////////////
-  const starterItems = [
-    {
-      name: "Chicken breast",
-      quantity: 3,
-      price: 50,
-      description: "3x 10lb containers of chicken breast, expires on 12/11/21",
-      img: "https://static01.nyt.com/images/2020/05/01/science/01TB-CHICKEN/01TB-CHICKEN-videoSixteenByNineJumbo1600.jpg"
-    },
-    {
-      name: "Cheese",
-      quantity: 9,
-      price: 45,
-      description: "A fine lookin' large hunk-o-stinky cheese. 20lbs by weight.",
-      img: "https://cheese.com/media/uploads/cheese.com/2021/11/versions/cut%20wheels%20of%20cheese-w343.webp"
-    },
-    {
-      name: "Dark Roast Coffee",
-      quantity: 1,
-      price: 15,
-      description: "A huge 20lb sack of coffee beans. Don't need anymore. $15 OBO, no solicitors.",
-      img: "https://m.media-amazon.com/images/I/81DLJc5I5XL._SX522_.jpg"
-    },
-    {
-      name: "Moo milk",
-      quantity: 1,
-      price: 1,
-      description: "A gallon of milk. Hurry! Offer (and milk) expires tomorrow!",
-      img: "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png"
-    }
-  ];
-
+const starterItems = [
+  {
+    name: "Chicken breast",
+    quantity: 3,
+    price: 50,
+    description: "3x 10lb containers of chicken breast, expires on 12/11/21",
+    img: "https://static01.nyt.com/images/2020/05/01/science/01TB-CHICKEN/01TB-CHICKEN-videoSixteenByNineJumbo1600.jpg"
+  },
+  {
+    name: "Cheese",
+    quantity: 9,
+    price: 45,
+    description: "A fine lookin' large hunk-o-stinky cheese. 20lbs by weight.",
+    img: "https://cheese.com/media/uploads/cheese.com/2021/11/versions/cut%20wheels%20of%20cheese-w343.webp"
+  },
+  {
+    name: "Dark Roast Coffee",
+    quantity: 1,
+    price: 15,
+    description: "A huge 20lb sack of coffee beans. Don't need anymore. $15 OBO, no solicitors.",
+    img: "https://m.media-amazon.com/images/I/81DLJc5I5XL._SX522_.jpg"
+  },
+  {
+    name: "Moo milk",
+    quantity: 1,
+    price: 1,
+    description: "A gallon of milk. Hurry! Offer (and milk) expires tomorrow!",
+    img: "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png"
+  }
+]
 /////////
 //AUTH ROUTERS
 /////////
-
 app.get("/", auth, (req,res)=>{
   res.json(req.payload)
 })
-
 app.use("/auth", AuthRouter);
-
-
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
+app.get("/items", auth, async (req, res) => {
+  try {
+    const {username} = req.payload
+    res.status(200).json(await Item.find({username})) // all the item for the user w/ that username
+  }
+  catch(error){
+    res.status(400).json({error})
+  }
+})
 // SEED route for testing
 app.get("/items/seed", async (req, res) => {
   try {
@@ -144,50 +119,43 @@ app.get("/items/seed", async (req, res) => {
     res.status(400).json(error)
   }
 })
-
-app.get("/", (req, res) => {
-  res.send("ayyyy")
-})
-
-//  INDEX ROUTE
-app.get("/items", async (req, res) => {
-  try {
-    res.json(await Item.find({}))
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
-  
+// //  INDEX ROUTE
+// app.get("/items", auth, async (req, res) => {
+//   try {
+//     res.json(await Item.find({}))
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
 // CREATE ROUTE
-app.post("/items", async (req, res) => {
+app.post("/items", auth, async (req, res) => {
   try {
-    res.json(await Item.create(req.body))
+    const {username} = req.payload
+    req.body.username = username
+    res.status(200).json(await Item.create(req.body))
   } catch (error) {
     res.status(400).json(error)
   }
 })
-
 // UPDATE ROUTE
-app.put("/items/:id", async (req, res) => {
+app.put("/items/:id", auth, async (req, res) => {
   try {
-    res.json(await Item.findByIdAndUpdate(req.params.id, req.body, { new: true }))
+    const {username} = req.payload
+    req.body.username = username
+    res.status(200).json(await Item.findByIdAndUpdate(req.params.id, req.body, { new: true }))
   } catch (error) {
     res.status(400).json(error)
   }
 })
-
 // DESTROY ROUTE
-app.delete("/items/:id", async (req, res) => {
+app.delete("/items/:id", auth, async (req, res) => {
   try {
-    res.json(await Item.findByIdAndRemove(req.params.id, req.body))
+    res.status(200).json(await Item.findByIdAndRemove(req.params.id, req.body))
   } catch (error) {
     res.status(400).json(error)
   }
 })
-
-
 ///////////////////////////////
 // LISTENER
 ////////////////////////////////
-
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
